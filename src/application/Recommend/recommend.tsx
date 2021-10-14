125438c25706daea725ef00d56fe17f43e4241e6
Ken-HH24 <1098862703@qq.com>
2021-10-14 21:34:46 +0800

    fix: white background and font problem

--- src/application/Recommend/recommend.tsx
+++ src/application/Recommend/recommend.tsx
@@ -5,7 +5,7 @@ import RecommendList from '../../components/RecommendList';
 
 const recommendList = [
     {
-        imgUrl: 'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/17c622fc1e4b4a39949363ec8bf9d3fc~tplv-k3u1fbpfcp-zoom-crop-mark:1304:1304:1304:734.awebp',
+        imgUrl: 'https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/f3299f4346c04040a60b435a64098854~tplv-k3u1fbpfcp-zoom-crop-mark:1304:1304:1304:734.awebp?',
         desc: 'test test test asdfawheahaehaeh',
         count: 678
     },
